3c85579d60f572121c7529720116c9cbbf56b088
Omprakash <omprakashuppar123@gmail.com>
2024-12-17 16:44:26 +0530

    Test

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const { register, login,  logout } = require('../controllers/authController');
+const express = require("express");
+const { register, login, logout } = require("../controllers/authController");
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
+
+router.post("/logout", logout);
